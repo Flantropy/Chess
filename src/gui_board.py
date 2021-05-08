c256f8ea8b6ecb2acb0e27d3d6ce98260bcbef90
Flantropy <80071496+Flantropy@users.noreply.github.com>
2021-05-08 19:47:37 +0300

    changed gui board initialization
    Signed-off-by: Flantropy <80071496+Flantropy@users.noreply.github.com>

--- src/gui_board.py
+++ src/gui_board.py
@@ -1,30 +1,46 @@
+from typing import List, Optional
 import chess
-from typing import List
-from cell import Cell
+import chess.svg
 import pygame
-from res.constants import DIR_IMAGES, CELL_SIZE
+from cell import Cell
+from res.constants import CELL_SIZE, DIR_IMAGES
 
 
 class GUIBoard:
-	pawn_img = pygame.transform.scale(pygame.image.load(DIR_IMAGES + r"\w_pawn.png"), CELL_SIZE)
-	
-	def __init__(self, board: chess.Board):
-		self.board = board
-		self.__sprites = []
-	
-	@property
-	def sprites(self):
-		return self.__sprites
-	
-	@sprites.setter
-	def sprites(self, value: List[Cell]):
-		self.__sprites = value
-	
-	@classmethod
-	def get_cells(cls, fen: str) -> List[Cell]:
-		cells = []
-		for rank in chess.RANK_NAMES:
-			for file in chess.FILE_NAMES:
-				cells.append(Cell(rank=rank, file=file, piece=cls.pawn_img))
-		
-		return cells
+    def __init__(self, board: chess.Board):
+        self.board = board
+        self.__sprites = []
+        self.selected_cell: Optional[Cell] = None
+    
+    @property
+    def sprites(self):
+        return self.__sprites
+    
+    @sprites.setter
+    def sprites(self, value: List[Cell]):
+        self.__sprites = value
+    
+    @classmethod
+    def get_cells(cls) -> List[Cell]:
+        cells = []
+        for rank in chess.RANK_NAMES:
+            for file in chess.FILE_NAMES:
+                cells.append(Cell(rank=rank, file=file))
+        return cells
+    
+    def update_piece_positions(self, fen: str):
+        for cell in self.sprites:
+            cell.piece = None
+        pieces = dict(p="pawn", b="bishop", r="rook", n="knight", q="queen", k="king")
+        i = 0
+        for row in reversed(fen.split("/")):
+            for symbol in row:
+                if symbol.isdigit():
+                    i += int(symbol)
+                else:
+                    color = "w" if symbol.isupper() else "b"
+                    image = pygame.transform.scale(pygame.image.load
+                                                   (DIR_IMAGES + f"\\{color}_{pieces[symbol.lower()]}.png"),
+                                                   CELL_SIZE)
+                    self.sprites[i].piece = image
+                    i += 1
